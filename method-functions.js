89ad5eafbfe8b29209858046cc330925a744ee4a
Stephen Clark <stephen@californiaclarks.com>
2015-12-23 20:12:02 -0500

    Removed first past the post (fpp)

--- method-functions.js
+++ method-functions.js
@@ -6,13 +6,6 @@ module.exports = {
     });
     return winner
   },
-  fpp: function(votes) {
-    var winner = {name: "no winner", votes: 0};
-    votes.forEach(function(e,i,arr) {
-      if (e.votes > winner.votes) winner = e;
-    });
-    return winner
-  },
   okay: function(votes) {
     var winner = {name: "no winner", votes: 0};
     votes.forEach(function(e,i,arr) {
