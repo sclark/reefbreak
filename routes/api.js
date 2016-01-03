8e222bba72c559bac615ff137b489f34b6ec9e11
Stephen Clark <stephen@californiaclarks.com>
2016-01-02 20:51:50 -0800

    Implement API routes

--- routes/api.js
+++ routes/api.js
@@ -18,11 +18,32 @@ router.get('/poll/:name', function(req, res) {
 });
 
 router.get('/poll/:name/r', function(req, res) {
+  Poll.findOne({name: req.params.name}).exec(
+    function (e, poll) {
+      if (poll) res.send({options: poll.options, status: "success"});
+      else res.send({options: null, status: "error: poll not found"})
+    }
+  );
+});
+
+router.post('/poll/:name/cast', function(req, res) {
   res.send({});
 });
 
 router.post('/new', function(req, res) {
-  res.send({});
+  var poll = req.body;
+  if (method in poll && duration in poll && prompt in poll && options in poll && 'name' in poll) {
+    Method.findOne({code: poll.method}).exec(
+      function(e, method) {
+        if (e || method == null) res.send({status: "error: method not found"});
+        var poll = new Poll({created: new Date().getTime(), winner: "", time: poll.duration, single: method.single, prompt: poll.prompt, options: poll.options, votes: 0, name: poll.name, method: poll.method});
+        poll.save(function(e, d) {
+          if (e) res.send({status: "error: poll not created"});
+          else res.send({status: "success"});
+        });
+      }
+    );
+  }
 });
 
 module.exports = router;
